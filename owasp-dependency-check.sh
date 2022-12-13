9a04aa8a947c08a253f5885080902053a7b6b7c5
HarshithaKuppam <84552188+HarshithaKuppam@users.noreply.github.com>
2022-12-13 13:39:15 +0530

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -30,6 +30,6 @@ docker run --rm \
     --scan /src \
     --format "ALL" \
     --project "$DC_PROJECT" \
-    --out "/var/lib/jenkins/workspace/jenkins-maven/odc-reports"
+    --out "/var/lib/jenkins/workspace/jenkins-maven/odc-reports/"
     # Use suppression like this: (where /src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
