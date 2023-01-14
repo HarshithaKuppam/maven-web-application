c8b7bd5fa69c9c37aeb96a29301ed517b1d5912e
HarshithaKuppam <84552188+HarshithaKuppam@users.noreply.github.com>
2023-01-14 21:38:48 +0530

    Update reference.tf

--- TerraformScript/section2/reference.tf
+++ TerraformScript/section2/reference.tf
@@ -24,4 +24,15 @@ resource "aws_eip_association" "eip_assoc" {
   allocation_id = aws_eip.lb.id
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "security-group-one"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_eip.lb.public_ip/32]
+ 
+  }
 
