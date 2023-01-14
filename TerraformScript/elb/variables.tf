61bb978a433707502ed615ec507f15b242ca5c9f
HarshithaKuppam <84552188+HarshithaKuppam@users.noreply.github.com>
2023-01-14 23:31:47 +0530

    Update variables.tf

--- TerraformScript/elb/variables.tf
+++ TerraformScript/elb/variables.tf
@@ -1,7 +1,7 @@
 variable "elb_name"{}
 variable "az"{
 
- default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+ #default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   
 }
 variable "timeout"{}
